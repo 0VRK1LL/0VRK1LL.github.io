a13571f2d658d093456864c48387266e3d632743
chuchu01 <lukanikolic42@gmail.com>
2020-05-22 13:48:28 +0200

    workinprogressanim

---
+++ index2.js
@@ -0,0 +1,7 @@
+var animation=bodymovin.loadAnimation({
+    container: document.getElementById('workinprogress'),
+    renderer:'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animacija2.json'
+})
